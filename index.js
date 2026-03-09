725e318bbaf1c295ded9121ea214d9993777f9a8
Skugiijb546vi <67493589+Skugiijb546vi@users.noreply.github.com>
2026-03-09 10:53:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,11 @@ app.get('/api/get-link', async (req, res) => {
     try {
         browser = await puppeteer.launch({
             headless: true,
-            // 💡 شوێنی کرۆممان سڕییەوە بۆ ئەوەی خۆی ئۆتۆماتیکی بیدۆزێتەوە
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',
                 '--disable-accelerated-2d-canvas',
-                '--no-first-run',
                 '--no-zygote',
                 '--single-process',
                 '--disable-gpu'
